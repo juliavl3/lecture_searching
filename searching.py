b37aa5c3a1d769057bbbc254a45d9adba20ae2ba
Vlčanová Júlia (257066) <257066@vutbr.cz>
2024-04-08 12:48:22 +0200

    linear_search

--- searching.py
+++ searching.py
@@ -12,11 +12,26 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path) as file:
+        data = json.load(file)
+    return data[field]
+
+def linear_search(sequential_data, searching_number):
+    list = []
+    for index, number in enumerate(sequential_data):
+        if number == searching_number:
+            list.append(index)
+    count = len(list)
+    return {'positions':list,'count': len(list)}
+
+
+
 
 
 def main():
     sequential_data = read_data('sequential.json', 'unordered_numbers')
     print(sequential_data)
+    print(linear_search(sequential_data,searching_number=5))
 
 
 
