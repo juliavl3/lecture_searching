7cc1e8015738afc3bef6b9b6259cfccea899ae5c
Vlcanova Julia <257066@vutbr.cz>
2025-04-10 10:11:20 +0200

    search patern

--- searching.py
+++ searching.py
@@ -31,22 +31,49 @@ def linear_search(sequential_data, number):
     :param number:
     :return: dictionary with indices and occurrences of the number we are looking for
     """
-    dict_data = {}
+    dict_data = {"positions":[] ,"count": 0}
     index = []
     for i,val in enumerate(sequential_data):
         if val == number:
             index.append(i)
-            dict_data["positions"] = index
-        else:
-            continue
+        else:#keby mame break zhorsilo by to vypocetnu narocnost, teda teraz je narocnost O(n) linearne
+            continue #alebo vnorene for cykly = zhorsuje = On exponencialna na druhu
 
-        dict_data["count"] = len(index)
-
+    dict_data["count"] = len(index)
+    dict_data["positions"] = index
     return dict_data
 
+def pattern_search(seq_d, pattern):
+
+    #je to dobre ale vypocetne je to narocnejsie jak by to mohlo byt lahsie
+    pattern_indices = []
+    for i in range(0, len(pattern)):
+        kodon = ""
+        for idx,nucleotide in enumerate(seq_d):
+            kodon += nucleotide
+            if kodon == pattern:
+                pattern_indices.append(idx- 1)
+                kodon = kodon[1:]
+
+            if len(kodon) == len(pattern):
+                kodon = kodon[1:]
+
+            else:
+                continue
+
+    # indices = set()
+    # for idx_left in range(len(seq_d) - len(pattern)+1):
+    #     if seq_d[idx_left:idx_left + len(pattern)] == pattern:
+    #         indices.add(idx_left + len(pattern) // 2)
+
+    return set(pattern_indices)
+
 def main():
-    seq_data = read_data("sequential.json","unordered_numbers")
-    print(linear_search(seq_data, 9))
+    seq_data = read_data("sequential.json","dna_sequence")
+    dict_data = linear_search(seq_data, 9)
+    print(pattern_search(seq_data, "ATA"))
+
+
 
 if __name__ == '__main__':
     main()
